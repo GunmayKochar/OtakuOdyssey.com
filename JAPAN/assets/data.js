b494914ece75d9dc630eb9d10d276432c91ecf76
GunmayKochar <166414746+GunmayKochar@users.noreply.github.com>
2024-04-08 20:14:53 +0530

    Add files via upload

---
+++ JAPAN/assets/data.js
@@ -0,0 +1,53 @@
+// Locations used in for map markers
+
+let locations = [
+    {
+        coords: {lat: 35.6962, lng: 139.5704},
+        name: "Studio Ghibli Museum",
+        category: "animemanga",
+        website: "http://www.ghibli-museum.jp/en/"
+    },
+    {
+        coords: {lat: 35.7288, lng: 139.7192},
+        name: "Pokemon Center Mega Tokyo",
+        category: "animemanga",
+        website: "https://www.pokemon.co.jp/gp/pokecen/megatokyo/"
+    },
+    {
+        coords: {lat: 35.6586, lng: 139.7454},
+        name: "Tokyo One Piece Tower",
+        category: "animemanga",
+        website: "https://onepiecetower.tokyo/?lang=en"
+    },
+    {
+        coords: {lat: 35.6245, lng: 139.7755},
+        name: "Odaiba Gundam",
+        category: "animemanga",
+        website: "https://www.japanvisitor.com/japan-city-guides/gundam"
+    },
+    {
+        coords: {lat: 35.6620, lng: 139.6990},
+        name: "Nintendo Tokyo",
+        category: "videogames",
+        website: "https://www.nintendo.co.jp/officialstore/index.html"
+    },
+
+    {
+        coords: {lat: 35.6616, lng: 139.6982},
+        name: "VR Park Shibuya",
+        category: "videogames",
+        website: "https://www.adores.jp/vrpark/en/shibuya.html"
+    },
+    {
+        coords: {lat: 35.7050, lng: 139.7538},
+        name: "SEGA Tokyo Dome City",
+        category: "videogames",
+        website: "https://tempo.sega.jp/am/tokyo-domecity/"
+    },
+    {
+        coords: {lat: 35.7023, lng: 139.7745},
+        name: "Electric Town Akihabara Tokyo",
+        category: "videogames",
+        website: "https://www.gotokyo.org/en/spot/55/index.html"
+    },
+];
